0c0f192574fe915dd8f1258947296d74880cfe7b
Compy <hello@86pixels.com>
2018-02-26 20:03:43 -0600

    Initial Commit

---
+++ Ardemu/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Ardemu
+//
+//  Created by Compy on 2/26/18.
+//  Copyright © 2018 86pixels. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
